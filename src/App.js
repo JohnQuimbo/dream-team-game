f78003a4681411f3bb78fea316ad5704fc091608
John Quimbo <johnanthony.quimbo@gmail.com>
2020-04-23 02:14:19 -0400

    updated team component, playerSearchInput component, and changed styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Team from './Team'
 import { render } from '@testing-library/react';
 
 class App extends React.Component {
@@ -12,10 +13,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <Team name =  "Team 1" />
+        <Team name = "Team 2" />
       </div>
     );
   }
 }
-
+//need to test an input and retrieve the stats of a player based on the search of the user
 
 export default App;
