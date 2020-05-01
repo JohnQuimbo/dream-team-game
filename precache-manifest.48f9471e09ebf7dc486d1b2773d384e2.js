beb4dc069698bf0a24c83451c8e0c8862e666f03
John Quimbo <johnanthony.quimbo@gmail.com>
2020-05-01 16:24:01 -0400

    Updates

---
+++ precache-manifest.48f9471e09ebf7dc486d1b2773d384e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5add1629478f3a81fd33584ad2b661",
+    "url": "/dream-team-game/index.html"
+  },
+  {
+    "revision": "09bb6ad488b646065e56",
+    "url": "/dream-team-game/static/css/main.716bcedf.chunk.css"
+  },
+  {
+    "revision": "660cd094953646ef2e1c",
+    "url": "/dream-team-game/static/js/2.e647cc68.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/dream-team-game/static/js/2.e647cc68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09bb6ad488b646065e56",
+    "url": "/dream-team-game/static/js/main.95a6ccea.chunk.js"
+  },
+  {
+    "revision": "35ea9e70a36d17d2aca7",
+    "url": "/dream-team-game/static/js/runtime-main.fb4401af.js"
+  }
+]);
